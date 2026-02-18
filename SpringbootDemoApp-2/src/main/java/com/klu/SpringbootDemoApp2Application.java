8f72740349cd0eb2ea09e557556c5c2f164eb830
PASARLAPUDI ABHINAI KUMAR <2400030056@kluniversity.in>
2026-02-18 09:40:30 +0530

    11-02-2026

---
+++ SpringbootDemoApp-2/src/main/java/com/klu/SpringbootDemoApp2Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApp2Application.class, args);
+	}
+
+}
---
+++ SpringbootDemoApp-2/src/test/java/com/klu/SpringbootDemoApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
